2e4e7f7ba3a9cad3206fcd260d6e4502fb380046
Yagiz Gokce <yagizgokce@hotmail.com>
2022-10-10 00:50:03 +0300

    TravelListImp repository fixed

--- app/src/main/java/com/yagizgokce/travelguideapp/data/remote/repository/TravelListRepositoryImp.kt
+++ app/src/main/java/com/yagizgokce/travelguideapp/data/remote/repository/TravelListRepositoryImp.kt
@@ -4,8 +4,9 @@ import com.yagizgokce.travelguideapp.data.remote.ApiService
 import com.yagizgokce.travelguideapp.domain.model.AllTravelListModel
 import com.yagizgokce.travelguideapp.domain.repository.TravelListRepository
 import retrofit2.Response
+import javax.inject.Inject
 
-class TravelListRepositoryImp(private val apiService: ApiService):TravelListRepository {
+class TravelListRepositoryImp @Inject constructor (private val apiService: ApiService):TravelListRepository {
 
     override suspend fun getTravelData(category: String): Response<List<AllTravelListModel>> {
         return apiService.getTravelData(category)
